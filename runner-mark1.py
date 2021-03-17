a1bd7f968320e6e3ceec258e5ce66b49621324c7
xbm <for_robot@mail.ru>
2021-03-17 20:51:49 +0500

    Четырёх-параметорный ввывод таблицы умножения слегка подкрашенный.

--- runner-mark1.py
+++ runner-mark1.py
@@ -6,18 +6,19 @@ def prn_line(offset_x):
     str_height, str_width = os.popen('stty size', 'r').read().split()
     height = int(str_height)
     width = int(str_width)
-    if offset_x[0] >= width:
-        offset_x[0] = 0
-    tmp_str = " " * offset_x[0]
+    if offset_x >= width:
+        offset_x = 0
+    tmp_str = " " * offset_x
     tmp_str = tmp_str + "\033[32m" + "*" + "\033[0m"
-    tmp_str = tmp_str + " " * (width - offset_x[0])
+    tmp_str = tmp_str + " " * (width - offset_x)
     print("\033[10;0H", end='')
     print(tmp_str)
+    return offset_x
 
 
-x = [0]
+x = 0
 # x[0] = int(input("Введите X :"))
 while 1:
-    prn_line(x)
-    x[0] = x[0] + 1
+    x=prn_line(x)
+    x = x + 1
     time.sleep(0.1)
